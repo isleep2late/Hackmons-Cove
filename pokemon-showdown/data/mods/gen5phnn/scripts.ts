7212fbfb5a2e3149a3eaa07b57b43663693d5dc5
isleep2late <ryguy007@gmail.com>
2026-07-12 03:11:51 -0400

    Gen 5 No Nerfs: above level 100, only HP scales; other stats capped at their level 100 values

--- pokemon-showdown/data/mods/gen5phnn/scripts.ts
+++ pokemon-showdown/data/mods/gen5phnn/scripts.ts
@@ -1,3 +1,21 @@
 export const Scripts: ModdedBattleScriptsData = {
 	inherit: 'gen5',
-}
+	statModify(baseStats, set, statName) {
+		const tr = this.trunc;
+		let stat = baseStats[statName];
+		if (statName === 'hp') {
+			return tr(tr(2 * stat + set.ivs[statName] + tr(set.evs[statName] / 4) + 100) * set.level / 100 + 10);
+		}
+		const statLevel = Math.min(set.level, 100);
+		stat = tr(tr(2 * stat + set.ivs[statName] + tr(set.evs[statName] / 4)) * statLevel / 100 + 5);
+		const nature = this.dex.natures.get(set.nature);
+		if (nature.plus === statName) {
+			stat = this.ruleTable.has('overflowstatmod') ? Math.min(stat, 595) : stat;
+			stat = tr(tr(stat * 110, 16) / 100);
+		} else if (nature.minus === statName) {
+			stat = this.ruleTable.has('overflowstatmod') ? Math.min(stat, 728) : stat;
+			stat = tr(tr(stat * 90, 16) / 100);
+		}
+		return stat;
+	},
+};
