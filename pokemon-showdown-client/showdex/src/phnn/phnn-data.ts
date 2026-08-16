c8e10e85d1c3b6a8e024a4b217f229575da0fb50
isleep2late <ryguy007@gmail.com>
2026-08-16 00:01:11 -0400

    Beta snapshot from desktop button - 2026-08-16 00:01:11

--- pokemon-showdown-client/showdex/src/phnn/phnn-data.ts
+++ pokemon-showdown-client/showdex/src/phnn/phnn-data.ts
@@ -294,6 +294,12 @@ export default {
 				"type": "Shadow",
 				"category": "Status",
 				"ignoreImmunity": true
+			},
+			"kamehameha": {
+				"basePower": 0,
+				"type": "Fighting",
+				"category": "Physical",
+				"ignoreImmunity": true
 			}
 		},
 		"typeChart": {
